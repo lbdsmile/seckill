ebcb5b52c73b79d61c9baf36f834e599dd5d40c6
smile <lbd_smile@163.com>
2022-05-15 23:11:57 +0800

    自定义数据校验的注解

---
+++ src/main/java/com/smile/seckill/validator/IsMobile.java
@@ -0,0 +1,28 @@
+package com.smile.seckill.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 定义手机号校验的注解
+ */
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER })
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {IsMobileValidator.class })
+public @interface IsMobile {
+    boolean required() default true;
+
+    String message() default "手机号码格式错误";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+}
---
+++ src/main/java/com/smile/seckill/validator/IsMobileValidator.java
@@ -0,0 +1,34 @@
+package com.smile.seckill.validator;
+
+import com.smile.seckill.util.ValidatorUtil;
+import org.apache.commons.lang3.StringUtils;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+/**
+ * 实现手机号校验注解的方法
+ */
+public class IsMobileValidator implements ConstraintValidator<IsMobile, String> {
+
+    private boolean required = false;
+
+    @Override
+    public void initialize(IsMobile constraintAnnotation) {
+        required = constraintAnnotation.required();
+
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(required) {
+            return ValidatorUtil.isMobile(value);
+        }else {
+            if(StringUtils.isEmpty(value)) {
+                return true;
+            }else {
+                return ValidatorUtil.isMobile(value);
+            }
+        }
+    }
+}
