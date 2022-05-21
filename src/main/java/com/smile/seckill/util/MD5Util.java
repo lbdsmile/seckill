4880bbf472dc47493a80bccba9fb333d9988f819
smile <lbd_smile@163.com>
2022-05-21 15:10:44 +0800

    工具类提交

--- src/main/java/com/smile/seckill/util/MD5Util.java
+++ src/main/java/com/smile/seckill/util/MD5Util.java
@@ -56,9 +56,9 @@ public class MD5Util {
     }
 
     // 该静态类的测试方法
-    public static void main(String[] args) {
-        System.out.println(inputPassToFormPass("123456"));//d3b1294a61a07da9b49b6e22b2cbd7f9
+//    public static void main(String[] args) {
+//        System.out.println(inputPassToFormPass("123456"));//d3b1294a61a07da9b49b6e22b2cbd7f9
 //		System.out.println(formPassToDBPass(inputPassToFormPass("123456"), "1a2b3c4d"));
 //		System.out.println(inputPassToDbPass("123456", "1a2b3c4d"));//b7797cce01b4b131b433b6acf4add449
-    }
+//    }
 }
