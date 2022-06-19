fb164129a281a5aa71aa8f8fa5ca4f79bc5fc3b8
smile <lbd_smile@163.com>
2022-06-19 21:10:20 +0800

    全局异常优化

--- src/main/java/com/smile/seckill/exception/GlobalExceptionHandler.java
+++ src/main/java/com/smile/seckill/exception/GlobalExceptionHandler.java
@@ -6,11 +6,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import com.smile.seckill.result.CodeMsg;
 import com.smile.seckill.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.validation.BindException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 /**
  * 处理全局异常的切面定义
@@ -18,6 +22,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @ControllerAdvice
 @ResponseBody
 public class GlobalExceptionHandler {
+    private static Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
 	@ExceptionHandler(value=Exception.class)
 	public Result<String> exceptionHandler(HttpServletRequest request, Exception e){
 		// e.printStackTrace();
@@ -30,8 +36,12 @@ public class GlobalExceptionHandler {
 			ObjectError error = errors.get(0);
 			String msg = error.getDefaultMessage();
 			return Result.error(CodeMsg.BIND_ERROR.fillArgs(msg));
-		}else {
-			return Result.error(CodeMsg.SERVER_ERROR);
+		}else if(e instanceof JedisConnectionException) {
+            log.info("JedisConnectionException:" + e.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
+        }else {
+            log.info(e.getMessage());
+            return Result.error(CodeMsg.SERVER_ERROR);
 		}
 	}
 }
